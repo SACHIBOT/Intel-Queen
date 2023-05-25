1d9723a883eb99843bcad11116eeddec5feefe7b
Sachintha Rashan <91013948+SACHIBOT@users.noreply.github.com>
2023-05-25 21:15:11 +0530

    Create t2.js

---
+++ commands/t2.js
@@ -0,0 +1,20 @@
+const { youtubeDL2 } = require('@bochilteam/scraper');
+const { tlang, botpic, cmd, prefix, runtime, Config, formatp } = require('../lib');
+
+cmd({
+  pattern: "test2",
+  alias: ["t2"],
+  desc: "YT video Downloader using youtube link",
+  react: "🎥",
+  category: "downloader"
+},
+async (Void, citel, text) => {
+  try {
+    const data = await youtubeDL2(text);
+    console.log(data);
+    citel.reply(data); // JSON
+  } catch (err) {
+    console.log(err);
+    citel.reply(`❌ Got an error: ${err}`);
+  }
+});
